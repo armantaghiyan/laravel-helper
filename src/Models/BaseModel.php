c0eb7f61ee71725764463f3c24b43a9321f5008a
armantaghiyan <taghiyan98@gmail.com>
2024-06-17 21:40:54 +0330

    edit base model

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -7,52 +7,55 @@ use Arman\LaravelHelper\Api\StatusCodes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Storage;
 
-trait BaseModel
-{
-    public function correctImage(string $path, string|null $filename, $default = null, string $disk = null): ?string
-    {
-        if ($filename || $default) {
-            $filename = $filename ?? $default;
-            return Storage::disk($disk??config('helper.default_disk'))->url($path . $filename);
-        }
+trait BaseModel {
 
-        return null;
-    }
+	public function correctImage(string $path, string|null $filename, $default = null, string $disk = null): ?string {
+		if ($filename || $default) {
+			$filename = $filename ?? $default;
+			return Storage::disk($disk ?? config('helper.default_disk'))->url($path . $filename);
+		}
 
-    private function checkParam(mixed $value): bool
-    {
-        if ($value === null || $value === '')
-            return false;
+		return null;
+	}
 
-        return true;
-    }
+	private function checkParam(mixed $value): bool {
+		if ($value === null || $value === '')
+			return false;
 
-    public function scopePage(Builder $query, int $loadedCount, int $perPage = 20): Builder
-    {
-        return $query->skip($loadedCount)->limit($perPage);
-    }
+		return true;
+	}
 
-    public function scopeFilter(Builder $query, string $col, mixed $value): Builder
-    {
-        if ($this->checkParam($value)) {
-            if (gettype($value) === 'string' && str_contains($value, '%')) {
-                return $query->where($col, 'LIKE', $value);
-            }
+	public function scopePage(Builder $query, int $loadedCount, int $perPage = 20): Builder {
+		return $query->skip($loadedCount)->limit($perPage);
+	}
 
-            return $query->where($col, $value);
-        }
+	public function scopeFilter(Builder $query, string $col, mixed $value): Builder {
+		if ($this->checkParam($value)) {
+			if (gettype($value) === 'string' && str_contains($value, '%')) {
+				return $query->where($col, 'LIKE', $value);
+			}
 
-        return $query;
-    }
+			return $query->where($col, $value);
+		}
 
-    public function scopeFirstOrError(Builder $query, array $cols = ['*'], string $message = 'not fount item.'): mixed
-    {
-        $result = $query->first($cols);
+		return $query;
+	}
 
-        if (!$result) {
-            throw new ErrorMessageException($message, StatusCodes::Not_found);
-        }
+	/**
+	 * @throws ErrorMessageException
+	 */
+	public function scopeFirstOrError(Builder $query, array $cols = ['*'], string $message = 'not fount item.'): mixed {
+		$result = $query->first($cols);
 
-        return $result;
-    }
+		if (!$result) {
+			throw new ErrorMessageException($message, StatusCodes::Not_found);
+		}
+
+		return $result;
+	}
+
+	public function scopePage2(Builder $query, $perPage = 20) {
+		$offset = ((int)request('page', 1) - 1) * $perPage;
+		return $query->offset($offset)->limit($perPage);
+	}
 }
